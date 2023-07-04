22bd71f7b053fb49e50f84daac4f9c2214f88373
Your Name <you@example.com>
2023-07-04 13:59:42 +0100

    removed console log

--- src/components/MoodPickerDemo.tsx
+++ src/components/MoodPickerDemo.tsx
@@ -7,41 +7,18 @@ function MoodPickerDemo(): JSX.Element {
   const handleMoodChangeToHappy = () => {
     const nextMood = "happy";
     queueRerenderWithNewMoodValue(nextMood);
-    console.log(
-      "😀 queued a rerender with mood as",
-      nextMood,
-      "but in this render it is still",
-      moodValueFromCurrentRender
-    );
   };
 
   const handleMoodChangeToConfused = () => {
     const nextMood = "confused";
     queueRerenderWithNewMoodValue(nextMood);
-    console.log(
-      "🤔 queued a rerender with mood as",
-      nextMood,
-      "but in this render it is still",
-      moodValueFromCurrentRender
-    );
   };
 
   const handleMoodChangeToSad = () => {
     const nextMood = "sad";
     queueRerenderWithNewMoodValue(nextMood);
-    console.log(
-      "😢 queued a rerender with mood as",
-      nextMood,
-      "but in this render it is still",
-      moodValueFromCurrentRender
-    );
   };
-
-  console.log(
-    "Component is rendering with a mood value of",
-    moodValueFromCurrentRender
-  );
-
+  
   return (
     <>
       <h1>Mood Picker Demo</h1>
